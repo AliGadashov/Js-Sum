2273e63e6105c46257ed86e542e77fc4d6c89eb7
Ali Gadashov <gadashovali13@gmail.com>
2024-05-02 10:05:55 +0400

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,4 @@
+let firstNumber = prompt("Include some number: ") - 0;
+let secondNumber = prompt("Include some other number: ") - 0;
+let sum = firstNumber + secondNumber;
+alert(`Sum of your number ${sum}`)
